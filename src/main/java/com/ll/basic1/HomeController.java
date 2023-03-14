58c28b970fdea62a0553cf4105b5e73f04b080a8
YunJiW <dtroy371@gmail.com>
2023-03-14 14:35:07 +0900

    remove 추가

--- src/main/java/com/ll/basic1/HomeController.java
+++ src/main/java/com/ll/basic1/HomeController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -62,6 +63,27 @@ public class HomeController {
         lists.add(new Person(id,age,name));
 
         return id + "번째 추가";
+    }
+    @GetMapping("/home/removePerson")
+    @ResponseBody
+    public String removePerson(int id){
+
+        //조건에 맞는 게 있다면 삭제된다. 삭제된다면 true값을 가짐
+        boolean removed = lists.removeIf(Person -> Person.getId() == id);
+        //강사님 코드
+        if(removed == false){
+            return id + "번사람이 존재하지않습니다.";
+        }
+        /* for문으로 내가 짠거
+        for(int idx = 0; idx <lists.size();idx++){
+            if(lists.get(idx).getId() == id){
+                lists.remove(idx);
+                return id + "번 사람 제거";
+            }
+        }
+        return id + "번사람이 존재하지않습니다.";
+         */
+        return id + "번 사람이 삭제되었습니다.";
     }
 
     @GetMapping("/home/people")
