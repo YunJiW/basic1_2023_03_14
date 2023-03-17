eb143ac8baceb8850ce04b4b0d99560c08e44829
YunJiW <dtroy371@gmail.com>
2023-03-17 15:47:22 +0900

    EntityListener 추가

--- src/main/java/com/ll/basic1/boundedContext/article/entity/Article.java
+++ src/main/java/com/ll/basic1/boundedContext/article/entity/Article.java
@@ -1,12 +1,16 @@
 package com.ll.basic1.boundedContext.article.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
@@ -17,14 +21,17 @@ import static jakarta.persistence.GenerationType.IDENTITY;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
 public class Article {
 
     @Id
     @GeneratedValue(strategy =IDENTITY) //Auto_Increment
     private long id;
 
+    @CreatedDate
     private LocalDateTime createDate; // 데이터 생성날짜
 
+    @LastModifiedDate
     private LocalDateTime modifyDate; // 데이터 수정 날짜
     private String title;
 
