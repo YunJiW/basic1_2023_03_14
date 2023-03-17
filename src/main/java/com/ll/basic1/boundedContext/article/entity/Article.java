aa7db6688ae88df1fd9c9547c87ed3a29751a1ea
YunJiW <dtroy371@gmail.com>
2023-03-17 11:54:34 +0900

    service 기능및 repo추가

--- src/main/java/com/ll/basic1/boundedContext/article/entity/Article.java
+++ src/main/java/com/ll/basic1/boundedContext/article/entity/Article.java
@@ -2,21 +2,30 @@ package com.ll.basic1.boundedContext.article.entity;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
+import static jakarta.persistence.GenerationType.IDENTITY;
+
 @Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Article {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY) //Auto_Increment
+    @GeneratedValue(strategy =IDENTITY) //Auto_Increment
     private long id;
 
     private LocalDateTime createDate; // 데이터 생성날짜
 
-    private LocalDateTime modifydate; // 데이터 수정 날짜
+    private LocalDateTime modifyDate; // 데이터 수정 날짜
     private String title;
 
     private String body;
