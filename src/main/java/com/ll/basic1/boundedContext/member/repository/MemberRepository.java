14d4db2a0b232579de1df7e888e4242f2c817bfd
YunJiW <dtroy371@gmail.com>
2023-03-20 10:23:21 +0900

    한글인코딩수정

--- src/main/java/com/ll/basic1/boundedContext/member/repository/MemberRepository.java
+++ src/main/java/com/ll/basic1/boundedContext/member/repository/MemberRepository.java
@@ -1,45 +1,11 @@
 package com.ll.basic1.boundedContext.member.repository;
 
 import com.ll.basic1.boundedContext.member.entity.Member;
-import lombok.Getter;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.ArrayList;
-import java.util.List;
-
-@Getter
-@Repository
-public class MemberRepository {
-    private List<Member> memberList;
-
-    public MemberRepository() {
-        memberList = new ArrayList<>();
-        memberList.add(new Member("user1","1234"));
-        memberList.add(new Member("abc","12345"));
-        memberList.add(new Member("test","12346"));
-        memberList.add(new Member("love","12347"));
-        memberList.add(new Member("like","12348"));
-        memberList.add(new Member("giving","12349"));
-        memberList.add(new Member("thanks","123410"));
-        memberList.add(new Member("hello","123411"));
-        memberList.add(new Member("good","123412"));
-        memberList.add(new Member("peace","123413"));
-    }
+import java.util.Optional;
 
 
-    public Member findByUsername(String username) {
-        return memberList
-                .stream()
-                .filter(m -> m.getUsername().equals(username))
-                .findFirst()
-                .orElse(null);
-    }
-
-    public Member findById(long id) {
-        return memberList
-                .stream()
-                .filter(m -> m.getId() == id)
-                .findFirst()
-                .orElse(null);
-    }
+public interface MemberRepository extends JpaRepository<Member,Long> {
+    Optional<Member> findByUsername(String username);
 }
--- src/main/java/com/ll/basic1/boundedContext/member/service/MemberService.java
+++ src/main/java/com/ll/basic1/boundedContext/member/service/MemberService.java
@@ -25,26 +25,22 @@ public class MemberService {
     }
     //Service에 몰빵
     public RsData tryLogin1(String username, String password) {
-        List<Member> memberList = memberRepository.getMemberList();
+        Member member = memberRepository.findByUsername(username).orElse(null);
 
+        if (member == null){
+            return RsData.of("F-2", "%s(은)는 존재하지 않는 회원입니다.".formatted(username));
 
-        //아이디를 찾는다.
-        for(Member member : memberList){
-            //같으면 비밀번호까지 체크
-            if(member.getUsername().equals(username)){
-                if(member.getPassword().equals(password)){
-                    return RsData.of("S-1","%s 님 환영합니다".formatted(username));
-                }
-                else
-                    return RsData.of("F-1","비밀번호가 일치하지않습니다.");
-            }
         }
-        return RsData.of("F-2","%s 는 존재하지 않습니다.".formatted(username));
+        if(!member.getPassword().equals(password)){
+            return RsData.of("F-1","비밀번호가 일치하지 않습니다.");
+        }
+
+        return RsData.of("S-1","%s 님 환영합니다".formatted(username),member);
     }
 
     //findbyname으로 해보기
     public RsData tryLogin2(String username, String password) {
-        Member member = memberRepository.findByUsername(username);
+        Member member = memberRepository.findByUsername(username).orElse(null);
 
         //없는경우 존재하지않음 리턴
         if (member == null) {
@@ -60,10 +56,10 @@ public class MemberService {
     }
 
     public Member findByUsername(String username) {
-        return memberRepository.findByUsername(username);
+        return memberRepository.findByUsername(username).orElse(null);
     }
 
     public Member findById(long id) {
-        return memberRepository.findById(id);
+        return memberRepository.findById(id).orElse(null);
     }
 }
